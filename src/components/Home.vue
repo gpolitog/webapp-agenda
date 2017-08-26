ba6145c1ea0b25d18ca675836914a5f4fe0585a5
Israel Ériston <israeleriston@bitbucket.org>
2017-08-26 05:45:36 -0300

    created aside left menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
     view="lHh Lpr fff"
     :left-class="{'bg-grey-2': true}"
   >
-    <q-toolbar slot="header" class="glossy">
+    <q-toolbar slot="header" >
       <q-btn
         flat
         @click="$refs.layout.toggleLeft()"
@@ -13,8 +13,8 @@
       </q-btn>
 
       <q-toolbar-title>
-        Quasar App
-        <div slot="subtitle">Running on Quasar v{{$q.version}}</div>
+        E-Professor
+        <div slot="subtitle">Agenda pessoal do professor</div>
       </q-toolbar-title>
     </q-toolbar>
 
@@ -26,10 +26,17 @@
       -->
 
       <q-list no-border link inset-delimiter>
-        <q-list-header>Essential Links</q-list-header>
+
+        <q-list-header>
+          <q-item >
+            <img src="statics/no_user.png"  alt="no_user">
+            <label>Olá Prof. Israel</label>
+          </q-item>
+          <hr>
+        </q-list-header>
         <q-item @click="launch('http://quasar-framework.org')">
           <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+          <q-item-main label="Perfil" sublabel="Informações gerais e seu perfil" />
         </q-item>
         <q-item @click="launch('http://forum.quasar-framework.org')">
           <q-item-side icon="record_voice_over" />
@@ -37,33 +44,23 @@
         </q-item>
         <q-item @click="launch('https://gitter.im/quasarframework/Lobby')">
           <q-item-side icon="chat" />
-          <q-item-main label="Gitter Channel" sublabel="Quasar Lobby" />
+          <q-item-main label="Contatos" sublabel="Gerencie e organize seus contatos" />
         </q-item>
         <q-item @click="launch('https://twitter.com/quasarframework')">
           <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
+          <q-item-main label="Ultimas Notícias" sublabel="interaga com os alunos e fique por dentro de tudo" />
+        </q-item>
+        <q-item @click="launch('https://twitter.com/quasarframework')" class="fixed-bottom">
+          <q-item-side icon="exit_to_app" />
+          <q-item-main label="Sair" sublabel="Efeture logout do app" />
         </q-item>
       </q-list>
-    </div>
-
-    <!--
-      Replace following <div> with
-      <router-view /> component
-      if using subRoutes
-    -->
-    <div class="layout-padding logo-container non-selectable no-pointer-events">
-      <div class="logo" :style="position">
-        <img src="~assets/quasar-logo-full.svg">
-      </div>
     </div>
   </q-layout>
 </template>
 
 <script>
 import {
-  dom,
-  event,
-  openURL,
   QLayout,
   QToolbar,
   QToolbarTitle,
@@ -75,24 +72,6 @@ import {
   QItemSide,
   QItemMain
 } from 'quasar'
-
-const
-  { viewport } = dom,
-  { position } = event,
-  moveForce = 30,
-  rotateForce = 40,
-  RAD_TO_DEG = 180 / Math.PI
-
-function getRotationFromAccel (accelX, accelY, accelZ) {
-  /* Reference: http://stackoverflow.com/questions/3755059/3d-accelerometer-calculate-the-orientation#answer-30195572 */
-  const sign = accelZ > 0 ? 1 : -1
-  const miu = 0.001
-
-  return {
-    roll: Math.atan2(accelY, sign * Math.sqrt(Math.pow(accelZ, 2) + miu * Math.pow(accelX, 2))) * RAD_TO_DEG,
-    pitch: -Math.atan2(accelX, Math.sqrt(Math.pow(accelY, 2) + Math.pow(accelZ, 2))) * RAD_TO_DEG
-  }
-}
 
 export default {
   name: 'index',
@@ -108,114 +87,13 @@ export default {
     QItemSide,
     QItemMain
   },
-  data () {
-    return {
-      orienting: window.DeviceOrientationEvent && !this.$q.platform.is.desktop,
-      rotating: window.DeviceMotionEvent && !this.$q.platform.is.desktop,
-      moveX: 0,
-      moveY: 0,
-      rotateY: 0,
-      rotateX: 0
-    }
-  },
-  computed: {
-    position () {
-      const transform = `rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg)`
-      return {
-        top: this.moveY + 'px',
-        left: this.moveX + 'px',
-        '-webkit-transform': transform,
-        '-ms-transform': transform,
-        transform
-      }
-    }
-  },
-  methods: {
-    launch (url) {
-      openURL(url)
-    },
-    move (evt) {
-      const
-        {width, height} = viewport(),
-        {top, left} = position(evt),
-        halfH = height / 2,
-        halfW = width / 2
-
-      this.moveX = (left - halfW) / halfW * -moveForce
-      this.moveY = (top - halfH) / halfH * -moveForce
-      this.rotateY = (left / width * rotateForce * 2) - rotateForce
-      this.rotateX = -((top / height * rotateForce * 2) - rotateForce)
-    },
-    rotate (evt) {
-      if (evt.rotationRate &&
-          evt.rotationRate.beta !== null &&
-          evt.rotationRate.gamma !== null) {
-        this.rotateX = evt.rotationRate.beta * 0.7
-        this.rotateY = evt.rotationRate.gamma * -0.7
-      }
-      else {
-        /* evt.acceleration may be null in some cases, so we'll fall back
-           to evt.accelerationIncludingGravity */
-        const
-          accelX = evt.acceleration.x || evt.accelerationIncludingGravity.x,
-          accelY = evt.acceleration.y || evt.accelerationIncludingGravity.y,
-          accelZ = evt.acceleration.z || evt.accelerationIncludingGravity.z - 9.81,
-          rotation = getRotationFromAccel(accelX, accelY, accelZ)
-
-        this.rotateX = rotation.roll * 0.7
-        this.rotateY = rotation.pitch * -0.7
-      }
-    },
-    orient (evt) {
-      if (evt.beta === null || evt.gamma === null) {
-        window.removeEventListener('deviceorientation', this.orient, false)
-        this.orienting = false
-
-        window.addEventListener('devicemotion', this.rotate, false)
-      }
-      else {
-        this.rotateX = evt.beta * 0.7
-        this.rotateY = evt.gamma * -0.7
-      }
-    }
-  },
-  mounted () {
-    this.$nextTick(() => {
-      if (this.orienting) {
-        window.addEventListener('deviceorientation', this.orient, false)
-      }
-      else if (this.rotating) {
-        window.addEventListener('devicemove', this.rotate, false)
-      }
-      else {
-        document.addEventListener('mousemove', this.move)
-      }
-    })
-  },
-  beforeDestroy () {
-    if (this.orienting) {
-      window.removeEventListener('deviceorientation', this.orient, false)
-    }
-    else if (this.rotating) {
-      window.removeEventListener('devicemove', this.rotate, false)
-    }
-    else {
-      document.removeEventListener('mousemove', this.move)
-    }
-  }
+  data: () => ({})
 }
 </script>
 
 <style lang="stylus">
-.logo-container
-  width 255px
-  height 242px
-  perspective 800px
-  position absolute
-  top 50%
-  left 50%
-  transform translateX(-50%) translateY(-50%)
-.logo
-  position absolute
-  transform-style preserve-3d
+img
+  text-aligner center
+label
+  margin 0 auto
 </style>
